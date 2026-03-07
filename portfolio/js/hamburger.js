bc355606085722d58ef1cf9bda576acb59898aff
shanevieira-rgb <shane.vieira@sjsu.edu>
2026-03-06 17:36:55 -0800

    Delete portfolio directory
    I want to get ried of this

--- portfolio/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-// console.log ("can you see this");
-
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav-menu")
-
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle("active")
-});
